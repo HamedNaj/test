1b0bfe6aa04cb496ea378592b98f1db04ad60538
Hamed Najafi <hamed.najafi.haghi@gmail.com>
2021-06-07 01:01:46 +0430

    a

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 
-const publicPath = path.join(__dirname, 'client/build');
+const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
